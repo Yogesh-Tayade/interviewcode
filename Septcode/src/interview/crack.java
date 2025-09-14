0368a696805066ac48234ed84168f79ea7f2d230
Yogesh <eryogeshtaayde@gmail.com>
2025-09-14 15:13:12 +0530

    add second java code as remthe dupcharfromstring

--- Septcode/src/interview/crack.java
+++ Septcode/src/interview/crack.java
@@ -17,10 +17,24 @@ public class crack {
 		}
 		System.out.println(rev);
 	}
+	void remduplicatechrinstring()
+	{
+		String str="programming";
+		String result="";
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			if(result.indexOf(ch)==-1)
+			{
+				result=result+ch;
+			}
+		}
+		System.out.println(result);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		crack obj=new crack();
-		obj.mirror();
+		obj.remduplicatechrinstring();
 	}
 
 }
